69d4706dde7117f63c3ab0ceb5e696d40a9ca2e4
Gareth Robinson <garethr1@yahoo.co.uk>
2018-02-10 21:40:38 +0000

    Separating dom element defaults and audio graph defaults into its own function

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
-var createModel = require('./create-model')
-var createAudioGraph = require('./create-audio-graph')
-var AudioGraphControl = require('./AudioGraphControl')
-var CanvasControl = require('./CanvasControl')
-var connectListeners = require('./connect-listeners')
+window.onload = function () {
+  var createModel = require('./create-model')
+  var createAudioGraph = require('./create-audio-graph')
+  var AudioGraphControl = require('./AudioGraphControl')
+  var CanvasControl = require('./CanvasControl')
+  var connectListeners = require('./connect-listeners')
+  var initialiseValues = require('./initialise-values')
 
-var model = createModel()
-var audioGraph = createAudioGraph(model)
-var graphControl = new AudioGraphControl(audioGraph, model)
-var canvasControl = new CanvasControl(model)
-connectListeners(model)
+  var model = createModel()
+  var audioGraph = createAudioGraph()
+  var graphControl = new AudioGraphControl(audioGraph, model)
+  var canvasControl = new CanvasControl(model)
+  initialiseValues(audioGraph, model)
+  connectListeners(model)
 
-var flop = false
-setInterval(function () {
-  graphControl.update()
-  if (flop) {
-    canvasControl.update()
-  }
-  flop = !flop
-}, 20)
+  var flop = false
+  setInterval(function () {
+    graphControl.update()
+    if (flop) {
+      canvasControl.update()
+    }
+    flop = !flop
+  }, 20)
+}
